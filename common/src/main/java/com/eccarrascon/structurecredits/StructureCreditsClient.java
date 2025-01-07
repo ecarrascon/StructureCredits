bc43e7c876be7037643d8096168b26c7d44f25ff
ECarrascon <carrascon.eduardo@gmail.com>
2025-01-07 14:32:43 +0100

    Refactor

--- common/src/main/java/com/eccarrascon/structurecredits/StructureCreditsClient.java
+++ common/src/main/java/com/eccarrascon/structurecredits/StructureCreditsClient.java
@@ -5,7 +5,7 @@ import dev.architectury.registry.client.keymappings.KeyMappingRegistry;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
-import static com.eccarrascon.structurecredits.registry.KeyMapRegistry.*;
+
 
 @Environment(EnvType.CLIENT)
 public class StructureCreditsClient {
